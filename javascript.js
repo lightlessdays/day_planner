ec46af5980f73e5f99ba1efd28d4dbea0be88ed2
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-09-14 13:24:08 +0930

    add momentjs time to header

--- javascript.js
+++ javascript.js
@@ -1,14 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function(){
+
+    var hour;
+    var timeInterval;
 // header section
-// display day of week
+// display time and update every second
+timeInterval = setInterval(function(){
+    // display day of week
 $('#day-display').text(moment().format('dddd'));
 // display time of day
 $('#time-digits').text(moment().format('h:mm'));
 // display am / pm for style purposes
 $('#time-period').text(moment().format('a'));
+// save time to variable
+hour = moment().format('H');
 
 
+// add if statements for before / after
+if ($( '.timeblock' ).val() > hour){
+    
+}
+// timeblocks
 
+}, 100);
 
 
 
