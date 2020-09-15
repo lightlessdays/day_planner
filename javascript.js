514baa3e43aefe55538a83c4e18bebad7d3d37ca
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-09-15 12:22:36 +0930

    update UI, add functional current time line

--- javascript.js
+++ javascript.js
@@ -13,14 +13,32 @@ $('#time-digits').text(moment().format('h:mm'));
 $('#time-period').text(moment().format('a'));
 // save time to variable
 hour = moment().format('H');
-
+var minutes = moment().format('mm');
 
 // add if statements for before / after
-if ($( '.timeblock' ).val() > hour){
-    
-}
-// timeblocks
 
+// timeblocks
+switch (hour){
+    case 9:
+        $('#9').removeClass("past future").addClass("present");
+        $('#10').removeClass("past present").addClass("future");
+        $('#11').removeClass("past present").addClass("future");
+        $('#12').removeClass("past present").addClass("future");
+        $('#13').removeClass("past present").addClass("future");
+        $('#14').removeClass("past present").addClass("future");
+        $('#15').removeClass("past present").addClass("future");
+        $('#16').removeClass("past present").addClass("future");
+        break;
+}
+// current time display
+if (hour > 17 || hour < 9){
+    $('#current-time').css("display", "none");
+} else {
+    $('#current-time').css("display", "grid");
+}
+// current time line position
+var position = ((hour * 100) - 802) + (minutes * 1.65);
+ $('#current-time').css('top', position+'px');
 }, 100);
 
 
