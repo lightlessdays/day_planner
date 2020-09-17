72b320dcf260634088380d5e1315dbdd0f5d89fb
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-09-17 12:49:58 +0930

    working site, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # wk5_day_planner
- Day planner website
+## Overview
+ This is a website that uses jquery, moment.js and local storage to create a day planner.
+ ![screenshot of main page](./screenshots/)
+ [Click here to go to site](https://nick75mowbray.github.io/wk5_day_planner/)
+ ### Features
+ Display of current day and time.\
+ Current time indicator that changes position on the page depending on the time.\
+ Information entered by the user is stored in local storage.
+ ### Screenshots
--- javascript.js
+++ javascript.js
@@ -2,54 +2,54 @@ $(document).ready(function(){
 
     var hour;
     var timeInterval;
-// header section
-// display time and update every second
-timeInterval = setInterval(function(){
-    // display day of week
-$('#day-display').text(moment().format('dddd'));
-// display time of day
-$('#time-digits').text(moment().format('h:mm'));
-// display am / pm for style purposes
-$('#time-period').text(moment().format('a'));
+    // header section
+    // display time and update every second
+    timeInterval = setInterval(function(){
+        // display day of week
+    $('#day-display').text(moment().format('dddd'));
+    // display time of day
+    $('#time-digits').text(moment().format('h:mm'));
+    // display am / pm for style purposes
+    $('#time-period').text(moment().format('a'));
 
-// save time to variable
-hour = parseInt(moment().format('H'));
-var minutes = parseInt(moment().format('mm'));
+    // save time to variable
+    hour = parseInt(moment().format('H'));
+    var minutes = parseInt(moment().format('mm'));
 
-// add if statements for before / after
-
-// timeblocks
-if (hour < 9){
-    $('.time-block').removeClass("present past").addClass("future");
-}
-if (hour > 16){
-    $('.time-block').removeClass("present future").addClass("past");
-}
-$( "div.time-block" ).each(function() {
-    var timeblockNum = parseInt($( this ).data("value"));
-    if (hour < timeblockNum){
-        $( this ).removeClass("past present").addClass("future");
+    // timeblocks
+    if (hour < 9){
+        $('.time-block').removeClass("present past").addClass("future");
     }
-    if (hour == timeblockNum){
-        $( this ).removeClass("past future").addClass("present");
+    if (hour > 16){
+        $('.time-block').removeClass("present future").addClass("past");
     }
-    if (hour > timeblockNum){
-        $( this ).removeClass("present future").addClass("past");
-    }
-    
-  });
+    $( "div.time-block" ).each(function() {
+        var timeblockNum = parseInt($( this ).data("value"));
+        if (hour < timeblockNum){
+            $( this ).removeClass("past present").addClass("future");
+        }
+        if (hour == timeblockNum){
+            $( this ).removeClass("past future").addClass("present");
+        }
+        if (hour > timeblockNum){
+            $( this ).removeClass("present future").addClass("past");
+        }
+        
+    });
 
-// current time display
-if (hour > 17 || hour < 9){
-    $('#current-time').css("display", "none");
-} else {
-    $('#current-time').css("display", "grid");
-}
-// current time line position
-var position = ((hour * 100) - 802) + (minutes * 1.65);
- $('#current-time').css('top', position+'px');
+    // current time display
+    if (hour > 17 || hour < 9){
+        $('#current-time').css("display", "none");
+    } else {
+        $('#current-time').css("display", "grid");
+    }
+    // current time line position
+    var position = ((hour * 100) - 802) + (minutes * 1.65);
+    $('#current-time').css('top', position+'px');
 
 }, 100);
+
+
 
 // timeblocks object
 var blocks = {
@@ -95,6 +95,10 @@ var blocks = {
     }
 };
 
+
+// LOCAL STORAGE
+
+
 var storageKey = localStorage.getItem("storageKey");
 init();
 
@@ -103,6 +107,9 @@ function renderBlocks(){
         var timeblockNum = $( this ).attr("data-value");
         console.log(timeblockNum);
         console.log(this);
+        // clear old text (need when saving)
+        $( this ).empty();
+        // add text to timeblocks
         $( this ).append($("<h4 class='title'>"+blocks["block"+timeblockNum].title+"</h4>"));
         $( this ).append($("<p class='description'>"+blocks["block"+timeblockNum].description+"</p>"));
         if (blocks["block"+timeblockNum].title!==""||blocks["block"+timeblockNum].description!==""){
@@ -112,43 +119,43 @@ function renderBlocks(){
 };
 
 function init() {
-    // Write code here to check if there are todos in localStorage
-    // If so, parse the value from localStorage and assign it to the todos variable
+    // check if local storage has been used else get data from local storage
     if(storageKey===null){
         console.log("nothing in storage");
     } else {
         blocks = JSON.parse(localStorage.getItem("storageKey"));
     }
-    // Render todos to the DOM
+    // Render event text
     renderBlocks();
 };
 
 function storeBlocks() {
-    // Add code here to stringify the todos array and save it to the "todos" key in localStorage
+    // store timeblock objects in local storage
     localStorage.setItem("storageKey", JSON.stringify(blocks));
 };
 
 // declare variables for functions
-var timeblockthis;
 var blockNum;
 
 // display form and info for selected time block
 function addText(timeblockdiv){
-    // display form
-    $('#text-form').css('display', 'block');
-    
+    // get the selected time block number from the div.time-block data-value
     blockNum = ( $( timeblockdiv ).attr("data-value")).toString();
-        
+    // display values for selected timeblock in form
     $('#form-time').text(blocks["block"+blockNum].time);
     $('#title').val(blocks["block"+blockNum].title);
     $('#description').val(blocks["block"+blockNum].description) ;       
 };
 
 
+// EVENTS
+
+
 // timeblock on click display form
 $('.time-block').on("click", function(){
+    // display form
+    $('#text-form').css('display', 'block');
     addText(this);
-    timeblockthis = $( this );
 });
 
 // close button
