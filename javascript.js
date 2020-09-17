56820c861748bdda1815e1a856872764cfd1f4a4
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-09-17 09:49:56 +0930

    added timeblock object, working timedisplay on form

--- javascript.js
+++ javascript.js
@@ -50,23 +50,94 @@ var position = ((hour * 100) - 802) + (minutes * 1.65);
  $('#current-time').css('top', position+'px');
 }, 100);
 
+var blocks = {
+    block9: {
+        time: "9AM - 10AM",
+        title: "",
+        description: ""
+    },
+    block10: {
+        time: "10AM - 11AM",
+        title: "",
+        description: ""
+    },
+    block11: {
+        time: "11AM - 12PM",
+        title: "",
+        description: ""
+    },
+    block12: {
+        time: "12PM - 1PM",
+        title: "",
+        description: ""
+    },
+    block13: {
+        time: "1PM - 2PM",
+        title: "",
+        description: ""
+    },
+    block14: {
+        time: "2PM - 3PM",
+        title: "",
+        description: ""
+    },
+    block15: {
+        time: "3PM - 4PM",
+        title: "",
+        description: ""
+    },
+    block16: {
+        time: "4PM - 5PM",
+        title: "",
+        description: ""
+    }
+};
+
+console.log(blocks);
 
 
 
-
-
-});
-
-function addText(){
+function addText(timeblockdiv){
     // display form
     $('#text-form').css('display', 'block');
-
-    $('#form-time').text(this.data("time"));
-}
+    console.log(timeblockdiv);
+    console.log($( timeblockdiv ).attr("data-value"));
+    console.log(blocks.block9);
+    // console.log(blocks.block9.time);
+        switch ( $( timeblockdiv ).attr("data-value")){
+                case "9":
+                    $('#form-time').text(blocks.block9.time);
+                    break;
+                case "10":
+                    $('#form-time').text(blocks.block10.time);
+                    break;
+                case "11":
+                    $('#form-time').text(blocks.block11.time);
+                    break;
+                case "12":
+                    $('#form-time').text(blocks.block12.time);
+                    break;
+                case "13":
+                    $('#form-time').text(blocks.block13.time);
+                    break;
+                case "14":
+                    $('#form-time').text(blocks.block14.time);
+                    break;
+                case "15":
+                    $('#form-time').text(blocks.block15.time);
+                    break;
+                case "16":
+                    $('#form-time').text(blocks.block16.time);
+                    break;
+            default:
+                break;
+        }
+};
 
 $('.time-block').on("click", function(){
-    addText();
-
-
+    addText(this);
 
 });
+
+
+}); // document ready
